8a304ba55fe9b505294e873eac6695e9ff96df42
Rick van Schijndel <rol3517@gmail.com>
2017-11-24 15:23:09 +0100

    Now you can compile from command line with gradle, didn't understand the onClick handlers in Kotlin

--- app/src/main/java/com/myapplication/rick/packagedelivery/MainActivity.kt
+++ app/src/main/java/com/myapplication/rick/packagedelivery/MainActivity.kt
@@ -13,23 +13,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun onSaveFormatClick(view: View) {
-        val streets = ArrayList<Street>()
-        streets.add(Street("Trentstraat", Range(1, 40, RangeType.All), Direction.HighToLow))
-        streets.add(Street("Leekbusweg", Range(1, 15, RangeType.All)))
-        streets.add(Street("Langeboomstraat", Range(1, 89, RangeType.Uneven)))
-        streets.add(Street("Langeboomstraat", Range(2, 110, RangeType.Even), Direction.HighToLow))
-        streets.add(Street("Vingerhoedspat", Range(1,2, RangeType.All)));
+        btn_save_format.setOnClickListener(View.OnClickListener { _ ->
+            val streets = ArrayList<Street>()
+            streets.add(Street("Trentstraat", Range(1, 40, RangeType.All), Direction.HighToLow))
+            streets.add(Street("Leekbusweg", Range(1, 15, RangeType.All)))
+            streets.add(Street("Langeboomstraat", Range(1, 89, RangeType.Uneven)))
+            streets.add(Street("Langeboomstraat", Range(2, 110, RangeType.Even), Direction.HighToLow))
+            streets.add(Street("Vingerhoedspat", Range(1,2, RangeType.All)));
 
-        val routeFormat = RouteFormat("Test 4 Rick", streets)
-        val routeFormatWriter = RouteFormatWriter(this)
-        try {
-            routeFormatWriter.writeStreets(routeFormat)
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
+            val routeFormat = RouteFormat("Test 4 Rick", streets)
+            val routeFormatWriter = RouteFormatWriter(this)
+            try {
+                routeFormatWriter.writeStreets(routeFormat)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        })
+        btn_load_format.setOnClickListener(View.OnClickListener { _ ->
+            fillView()
+        })
     }
 
     private fun noRouteFormatFound() {
@@ -62,9 +65,5 @@ class MainActivity : AppCompatActivity() {
             e.printStackTrace()
         }
         return null
-    }
-
-    fun onLoadFormatClick(view: View) {
-        fillView()
     }
 }
