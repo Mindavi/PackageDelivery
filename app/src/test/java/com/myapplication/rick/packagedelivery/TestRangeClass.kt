a794e103d8e9f2c768f40ff68fcea584a29b9fb4
Rick van Schijndel <rol3517@gmail.com>
2018-01-05 10:45:05 +0100

    TestRangeClass: Move assignments of ranges to declarations

--- app/src/test/java/com/myapplication/rick/packagedelivery/TestRangeClass.kt
+++ app/src/test/java/com/myapplication/rick/packagedelivery/TestRangeClass.kt
@@ -7,21 +7,12 @@ import org.junit.Assert.*
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 class TestRangeClass {
-    private val lowerBoundUneven: Int
-    private val upperBoundUneven: Int
-    private val lowerBoundEven: Int
-    private val upperBoundEven: Int
-    private val zeroLowerBound: Int
-    private val zeroUpperBound: Int
-
-    init {
-        lowerBoundUneven = 1
-        upperBoundUneven = 89
-        lowerBoundEven = 2
-        upperBoundEven = 90
-        zeroLowerBound = 0
-        zeroUpperBound = 0
-    }
+    private val lowerBoundUneven: Int = 1
+    private val upperBoundUneven: Int = 89
+    private val lowerBoundEven: Int = 2
+    private val upperBoundEven: Int = 90
+    private val zeroLowerBound: Int = 0
+    private val zeroUpperBound: Int = 0
 
     @Test
     fun rangeCreationUnevenValid() {
